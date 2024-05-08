8c7c590a5e520419f8e4c905be14c07b839ec3fd
omrzcn <omrzcnn@gmail.com>
2024-05-08 12:26:44 +0300

    Derived and JPQL queries

--- src/main/java/com/cydeo/repository/AddressRepository.java
+++ src/main/java/com/cydeo/repository/AddressRepository.java
@@ -3,6 +3,7 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,12 @@ public interface AddressRepository extends JpaRepository<Address,Long> {
     List<Address> findAllByStreetStartingWith(String streetName);
 
     //Write a derived query to get top 3 addresses with a specific customer email
+    //???
+    List<Address> findDistinctTop3ByCustomer_Email(String email);
 
     //Write a JPQL query to get all addresses with a specific customerId
+
+    @Query("select a from Address a where a.customer.id=?1")
+    List<Address> retrieveAddressWithCustomerId(Long customer_id);
 
 }
